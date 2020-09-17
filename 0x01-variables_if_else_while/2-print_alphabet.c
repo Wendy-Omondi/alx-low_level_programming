7d8b543e123d3eb24b9b6aed127d7193390ce371
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-17 16:09:06 +0000

    Lowercase print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int w;
+for (w = 'a'; w <= 'z'; w++)
+{
+putchar(w);
+putchar('\n');
+}
+return (0);
+}
