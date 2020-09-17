f982932ee66315cdded76e02d919c430d3a7864d
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-17 17:08:07 +0000

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int w;
+for (w = 0; w <= 9; w++)
+{
+putchar (w);
+}
+int x;
+for (x = 'a'; x <= 'f'; x++)
+{
+putchar(x);
+}
+return (0);
+}
