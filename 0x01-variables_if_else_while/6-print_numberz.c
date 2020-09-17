d0bd0fde80f52a2db1b1966c92df57d307bc5308
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-17 16:45:16 +0000

    More numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int w;
+for (w = 0; w <= 10; w++)
+{
+putchar(w);
+putchar('\n');
+}
+return (0);
+}
