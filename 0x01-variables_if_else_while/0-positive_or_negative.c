ccc7c1249066f82960d0cccb63ddd3bad2ec883d
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-17 19:35:19 +0000

    Id else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-#include <stdio.h>
+
 /**
 * main - Entry point
 *
@@ -17,8 +17,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if
-(n > 0);
+if (n > 0)
 printf("%d is positive\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
