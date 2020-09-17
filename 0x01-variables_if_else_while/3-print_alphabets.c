69a5d5b0ce90e46ca8f856c74c88c07d5473f9ab
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-17 20:32:50 +0000

    lowercase UPPERCASE

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,10 @@ int main(void)
 char w;
 char y = '\n';
 for (w = 'a'; w <= 'z'; w++)
-{
-putchar (w);
-}
 int z;
 for (z = 'A'; z <= 'z'; z++)
-{
 putchar (w);
 putchar (z);
 putchar (y);
-}
 return (0);
 }
