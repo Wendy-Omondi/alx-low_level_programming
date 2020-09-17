0e685623cf35a151f59a04687338d1865482b927
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-17 20:57:57 +0000

    lowercase except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>           
+/**                          
+ * main - Entry point         
+ *                            
+ * Return: Always 0 (Success) 
+ */                           
+int main(void)               
+{  
+char w;
+char x = '\n';
+for w = 'a'; w <= 'e'; w++)
+putchar(w);
+puthcar(x);
+return (0);
+]
