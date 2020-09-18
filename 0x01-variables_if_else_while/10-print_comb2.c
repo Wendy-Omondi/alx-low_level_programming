8f31968bf7cb7aec4c628e2ef765d6a7353a48c7
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-18 11:04:34 +0000

    Print 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int w, y;
+for (w = '0'; w <= '9'; w++)
+{
+for (y = '0'; y <= '9'; y++)
+{
+putchar(w);
+putchar(y);
+if (w == '9' && y == '9')
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
