d51441ae79c6c759bbb1e6b6ad52c7351f5ee974
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-18 10:20:57 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@ int main(void)
 char w;
 char x;
 for (w = 0; w <= 9; w++)
-{
+
 putchar (w + '0');
-}
+
 int x;
 for (x = 'a'; x <= 'f'; x++)
-{
+
 putchar(x);
 putchar('\n');
-}
+
 return (0);
 }
