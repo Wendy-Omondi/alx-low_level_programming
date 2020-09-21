e69129091b2b44d7fd76167813e29576574cc936
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-21 20:14:00 +0000

    project header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,18 +1,6 @@
-#include <unistd.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+int _puthcar(char);
 
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-void print_alphabet_x10(void)
-{
-  return (0);
-}
+#endif
