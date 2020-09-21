511a192cb7ee140581f3c42fa4774013a5117272
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-21 12:15:03 +0000

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+void print_alphabet(void);
+{
+char w;
+for (w = 'a'; w <= 'z'; w++)
+putchar(w);
+}
+putchar('\n');
+return (0);
+}
