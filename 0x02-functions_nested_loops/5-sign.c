7fee4cc4573b25ea107d395f9bdfc25a07765447
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-22 21:39:16 +0000

    Assigning signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,25 @@
 #include "holberton.h"
 /**
 * print_sign - Entry point
-*
+*@n: interger
 * Return: Always 0 (Success)
 */
 
-int print_sign(int n);
+int print_sign(int n)
 {
-int n = 0;
-int w = '+';
-int y = '0';
-int x = '-';
 if (n > 0)
 {
-_putchar(w);
+_putchar('+');
 return (1);
 }
 else if (n == 0)
 {
-_putchar(y);
+_putchar ('0');
 return (0);
 }
-else if (n < 0)
+else
 {
-_putchar(x);
+_putchar('-');
 return (-1);
 }
 }
