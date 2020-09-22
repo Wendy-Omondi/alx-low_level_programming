7d4ae8c73b5f9c317f64664e27dc697179377f4f
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-22 11:54:39 +0000

    lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* main - Check the code for Hoberton school students
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char w;
+for (w = 'a'; w <= 'z'; w++)
+{
+_putchar(w);
+}
+_putchar('\n');
+return (0);
+}
