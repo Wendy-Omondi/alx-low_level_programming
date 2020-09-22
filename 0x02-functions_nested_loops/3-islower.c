d526a03ad2aaefa5b9a5cbae30988bbf36942296
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-22 18:47:44 +0000

    check lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
 /**
-* main - Check for lowercase character
-*
+* _islower - Check for lowercase character
+* 
 * Return: Always 0 (Success)
 */
-int main(void)
-{
-int _islower(int c);
 
-int ch = 'c';
+int _islower(int c)
+{
+int c;
 int w = '1';
 int y = '0';
-if (_islower(ch))
+if (_islower(c))
 _putchar(w);
 else
 _putchar(y);
 _putchar('\n');
-return (0);
+
 }
