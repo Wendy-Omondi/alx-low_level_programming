24e6dad9742868bb66275ef5dd4e7b53f856fc15
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-22 16:03:14 +0000

    Check for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* main - Checks for alphabetic character
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int _isalpha(int c);
+
+int c;
+
+c = _isalpha('H');
+_putchar(r + '1');
+c = _isalpha('o');
+_putchar(r + '1');
+c = _isalpha(108);
+_putchar(r + '1');
+c = _isalpha(';');
+_putchar(r + '0');
+_putchar('\n');
+return (0);
+}
