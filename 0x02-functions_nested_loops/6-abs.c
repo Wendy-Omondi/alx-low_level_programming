4f5c4fad66cfb9ddded3c99d12840318e105c3c2
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-22 17:24:07 +0000

    Absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int _abs(int);
+
+int w = _abs(-1);
+int y = _abs(0);
+int x = _abs(1);
+int z = _abs(-98);
+
+_putchar(w + '0');
+_putchar('\n');
+_putchar(y + '0');
+_putchar('\n');
+_putchar(x + '0');
+_putchar('\n');
+_putchar(z + '0');
+_putchar('\n');
+return (0);
+}
