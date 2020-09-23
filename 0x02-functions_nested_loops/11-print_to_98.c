da05cf2f625d62926a6e6899e67202f4e03ad729
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-23 18:55:06 +0000

    0-98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* print_to_98 - Prints all natural numbers from n to 98
+*@n: first number
+* Return: Always 0 (Success)
+*/
+void print_to_98(int n)
+{
+int w;
+if (n <= 98)
+{
+for (w = n; w < 98; w++)
+{
+printf("%w,", w);
+}
+}
+else
+{
+for (w = n; w > 98; w--)
+{
+printf("%w,", w);
+}
+printf("98\n");
+}
