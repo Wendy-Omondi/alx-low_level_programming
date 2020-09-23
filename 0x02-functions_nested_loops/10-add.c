d819526fc6e12d2c81a1c1f28d967c4e7384d05e
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-23 18:42:06 +0000

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* add - Adds two integers and returns the result
+*@w: first integer
+*@y: second integer
+* Return: Always 0 (Success)
+*/
+int add(int w, int y)
+{
+return (w + y);
+}
