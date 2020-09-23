b912a765f8a0673197dd3a5d33e7c0e2f00d0ea2
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-23 17:33:41 +0000

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+* times_table - Prints the 9 ties table starting with 0
+*
+* Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+int w, y prod;
+for (w = 0; w < 10; w++)
+{
+for (y = 0; y < 10; y++)
+{
+if (w == 0)
+{
+_putchar(48);
+continue;
+}
+prod = w * y;
+_putchar(',');
+_putchar(' ');
+if (prod >= 10)
+{
+_putchar(prod / 10 + 48);
+_putchar(prod % 10 + 48);
+}
+else
+{
+_putchar(' ');
+_putchar(prod + 48);
+}
+}
+_putchar('\n');
+}
+}
