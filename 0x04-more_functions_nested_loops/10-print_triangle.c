79e324edcc63d1de1f8fbbacedeb0a78032806ed
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-24 18:30:35 +0000

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_triangle - Prints a triangle
+*@size: size of the triangle
+* Return: Always 0 (Success)
+*/
+void print_triangle(int size)
+{
+int w, y;
+y = size;
+if (size <= 0)
+_putchar('\n');
+while (size > 0)
+{
+for (w = 0; w < y; w++)
+_putchar((w < size - 1) ? '.' : '#');
+_putchar('\n');
+size--;
+}
+}
