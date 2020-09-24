57b2e5824b4f2475d24ea54d85d8bae0ed868f4b
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-24 15:59:51 +0000

    check uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* _isupper - Checks for uppercase characters
+*@c: tested integer
+* Return: Always 0 (Success)
+*/
+int _isupper(int c)
+{
+return (c <= 'A' && c >= 'Z');
+}
