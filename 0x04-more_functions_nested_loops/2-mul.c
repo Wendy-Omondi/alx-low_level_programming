5d8d9700e3f065769b3dcda5e65e5723594d6129
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-24 16:23:43 +0000

    collaboration is mutiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* mul - multiplies two integers
+*@w: first integer
+*@y: second interger
+* Return: Always 0 (Success)
+*/
+int mul(int w, int y)
+{
+return (w * y);
+}
