2301b6bec848509693b99ccad1c979a8a24796f4
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-25 16:34:44 +0000

    Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,14 +8,18 @@ void print_square(int size)
 {
 int w, y;
 if (size <= 0)
+{
 _putchar('\n');
+}
 else
 {
 for (w = 0; w < size; w++)
 {
 for (y = 0; y < size; y++)
+{
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 }
