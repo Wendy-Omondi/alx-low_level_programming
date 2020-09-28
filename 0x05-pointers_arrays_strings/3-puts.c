f29a08e4d2d1381f547e990feb17cbc60084fd4b
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-09-28 16:09:20 +0000

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _puts - Entry point
+*@str: string to be printed
+* Return: Always 0 (Success)
+*/
+void _puts(char *str)
+{
+
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
