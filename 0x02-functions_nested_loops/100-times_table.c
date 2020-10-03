0d7a974620e31b1b78b21a46ee66805e50926a9e
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-03 20:51:55 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,14 +6,40 @@
 */
 void print_times_table(int n)
 {
-int i;
+int x, y, prod;
 
 if (n >= 15 || n < 0)
 {
 _putchar(0);
 }
-for (i = 0; i <= n; i++)
+for (x = 0; x <= n; x++)
 {
-_putchar(i);
+for (y = 0; y <= n; y++)
+{
+if (y == 0)
+{
+_putchar(48);
+continue;
+}
+}
+}
+prod = x * y;
+_putchar(',');
+_putchar(' ');
+
+if (prod >= 100)
+{
+_putchar(prod / 100 + 48);
+_putchar(prod / 10 + 48);
+_putchar(prod % 10 + 48);
+}
+else if (prod >= 10)
+{
+_putchar(prod / 10 + 48);
+_putchar(prod % 10 + 48);
+}
+else
+{
+_putchar(prod);
 }
 }
