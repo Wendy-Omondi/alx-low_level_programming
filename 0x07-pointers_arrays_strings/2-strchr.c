23cec1fca63b9324747836f74bea5b93b3b18337
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-05 07:08:53 +0000

    locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strchr - points to the first occurrence of the character c in the string s
+ *@s: string being pointed to
+ *@c: character to be located in the string
+ *Return: first occurrence of c in the string s, or NULL it not found
+ */
+char *_strchr(char *s, char c)
+{
+
+char w;
+
+while (1)
+{
+w = *s++;
+if (w == c)
+return (s - 1);
+if (!w)
+return (NULL);
+}
+}
