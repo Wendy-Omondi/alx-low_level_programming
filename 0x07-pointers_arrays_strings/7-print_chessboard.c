e1bb7d31d55e5eae99a8ba14e375ed2341a78808
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-05 16:28:53 +0000

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_chessboard - prints chesshboard
+ *@a: pointer to chessboard
+ *Return: Always (0);
+ */
+void print_chessboard(char (*a)[8])
+{
+int w, y;
+for (y = 0; y < 8; y++)
+{
+for (w = 0; w < 8; w++)
+{
+_putchar(a[y][w]);
+_putchar('\n');
+}
+}
+}
