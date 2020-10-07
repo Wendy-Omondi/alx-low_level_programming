38198c31d2c3c638055435826f51c1e179b258b4
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-07 18:30:15 +0000

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- *_sqrt_recursion - finds natural square root of a number
+ *issqt - finds natural square root of a number
  *
- *@n: integer whose square root is being found
+ *@n: squared number
  *
+ *@y: square root of number
  *Return: Integer
  */
 int issqt(int n, int y)
@@ -12,12 +13,18 @@ int issqt(int n, int y)
 if (y * y > n)
 return (-1);
 
-if (y * y <= n)
+if (y * y == n)
 return (y);
 
 return (y + 1);
 }
 
+/**
+ *_sqrt_recursion - returns natural square root of a number
+ *@n: squared number
+ *
+ *Return: integer
+ */
 int _sqrt_recursion(int n)
 
 {
