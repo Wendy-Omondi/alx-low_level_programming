36aef12835ff8889a043909bcb9de240feba3f77
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-07 07:41:06 +0000

    Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_puts_recursion - Prints a string
+ *
+ *@s: string to be printed
+ *
+ *Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+
+if (*s == '\0')
+
+_putchar('\n');
+return;
+{
+_putchar(*s);
+_puts_recursion(s++);
+}
+}
