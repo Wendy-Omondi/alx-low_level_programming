e569eb6f069d787f3bea3926367b615c5ea19d16
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-09 10:46:42 +0000

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *@argc: returns number of arguents in a function
+ *
+ *@argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+
+printf("%d\n", argc);
+
+{
+argv++;
+}
+return (0);
+
+}
