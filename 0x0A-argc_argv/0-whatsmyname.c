99fd016d7a666b2a7b48a803987aa48666a5e69d
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-09 09:49:33 +0000

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *@argc: argument count
+ *
+ *@argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+
+while (argc --)
+printf("%s\n", *argv++);
+return (0);
+
+}
