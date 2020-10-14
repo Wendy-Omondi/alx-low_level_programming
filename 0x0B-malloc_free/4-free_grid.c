f0b946e557669d8c0ee4608f030ef3800d7c4ef9
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-14 18:19:19 +0000

    free previous grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *free_grid - frees a previous two dimensional grid
+ *
+ *@grid: double pointer
+ *
+ *@height: height of grid
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+free(g[i];
+free(g);
+}
