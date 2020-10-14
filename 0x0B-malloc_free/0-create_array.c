c5020009501d2e9378d8cc4e28991fb1cb0ffeb3
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-14 17:18:47 +0000

    Character array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,27 @@
 char *create_array(unsigned int size, char c)
 {
 
-char *ptr = (char *)malloc(c);
+char *s;
 
 if (size == 0)
 {
 return (0);
 }
-return (ptr);
+
+s = malloc(sizeof(char) * size);
+
+if (s == NULL)
+{
+return (0);
+}
+
+else
+
+unsigned int i = 0;
+while (s[i] == '\0')
+{
+s[i] = c;
+i++;
+}
+return (s);
 }
