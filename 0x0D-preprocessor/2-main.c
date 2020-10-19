27d37d58772458bae36710a0a591658049381d08
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-19 08:42:45 +0000

    print filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  Prints the name of the file of the program was compiled from
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+
+printf("%s\n", __FILE__);
+
+return (0);
+
+}
