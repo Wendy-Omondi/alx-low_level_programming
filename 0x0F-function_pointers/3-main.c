2e1663eef11b3c86976f0ece537da215e1b257c5
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-22 12:15:13 +0000

    main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,21 +10,27 @@
 int main(int argc, char *argv[])
 {
 
-register int num1, num2, i;
+int num1, num2, i;
 int(*fp)(int, int);
 
 if (argc != 4)
 printf("Error\n");
 exit (98);
 
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 fp = get_op_func(argv[2]);
+
+if (fp == NULL)
+{
+printf("Error\n");
+exit (99);
+}
 
 if (!fp)
 printf("Error\n");
 exit (99);
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
 i = fp(num1, num2);
 
 printf("%d\n", i);
