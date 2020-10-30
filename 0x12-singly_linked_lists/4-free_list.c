685156c1e956b70ee3b51345398f2b2a100763d0
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-30 18:53:47 +0000

    frees previous list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,13 +6,11 @@
  */
 void free_list(list_t *head)
 {
-list_t *tmp;
 
-while (head)
+if (head != NULL)
 {
-tmp = head->next;
+free_list(head->next);
 free(head->str);
 free(head);
-head = tmp;
 }
 }
