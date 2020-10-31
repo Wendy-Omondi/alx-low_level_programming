e500fb2f3689dfb32257eb07c646d500bdef2eeb
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-31 18:13:29 +0000

    betty corrections

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,9 @@
 
 /**
  *print_list - prints all the elements of a list
- *list_t - size of list
- *Return: Number of nodes
- */                                                       
-                                                          
+ *@h: - the list_t
+ *Return: Number of nodes in h
+ */
 size_t print_list(const list_t *h)
 {
 int nodes = 0;
