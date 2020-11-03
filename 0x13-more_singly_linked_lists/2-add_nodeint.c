9f3c5f9dcca5019a52e4bfd021e19c36ce7b7438
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-03 17:25:57 +0000

    new node at the beginning of the list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,23 +6,23 @@
  *@n: the new integer to be added to the list
  *Return: address to the new element or NULL if failed
  */
-listint_t *add_nodeint(listint_t **head, const int n)        
-{                                                
+listint_t *add_nodeint(listint_t **head, const int n)
+{
 int i;
 listint_t *new;
 
 new = malloc(sizeof(listint_t));
 if (new == NULL)
 {
-    return NULL;
+return (NULL);
 }
 
 for (i = 0; i;)
 i++;
 new->n = n;
-new->next= *head;
+new->next = *head;
 
 *head = new;
 
-return (new);                                  
+return (new);
 }
