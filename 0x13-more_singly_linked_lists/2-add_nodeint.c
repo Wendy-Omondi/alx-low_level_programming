e8b184f94ba30dc7da3d4c2a7441b380ea08bc8d
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-03 17:14:14 +0000

    new node at the beginning of the list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *add_nodeint - adds a node at the begining of a list
+ *@head: a pointer to the hea of the listint_t
+ *@n: the new integer to be added to the list
+ *Return: address to the new element or NULL if failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)        
+{                                                
+int i;
+listint_t *new;
+
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+    return NULL;
+}
+
+for (i = 0; i;)
+i++;
+new->n = n;
+new->next= *head;
+
+*head = new;
+
+return (new);                                  
+}
