380cd7bbc98e62e9c5a4c5a15c51485e4be1090e
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-04 18:27:20 +0000

    insert new node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index - inserts a new node at a given position
+ *@head: pointer to the address of the head of the list
+ *@idx: index where new node will be added
+ *@n: data for the new node
+ *Return: address to new node otherwise NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+listint_t *temp = *head, *new;
+unsigned int i = 0;
+
+ new = malloc(sizeof(listint_t));
+new->n = n;
+
+if (idx == 0)
+{
+new->next = temp;
+*head = new;
+return (new);
+}
+
+while (i < idx-1)
+{
+if (temp == NULL || temp->next == NULL)
+return (NULL);
+temp = temp->next;
+}
+new->next = temp->next;
+temp->next = new;
+
+return (new);
+}
