b471c443cf4c9f88745de05489c9025266263f6c
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-05 17:13:37 +0000

    return value of a bit at a given position

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *get_bit - finds value of a bit at a given position
+ *@n: the value to check
+ *@index: position to get the value at
+ *Return: value of bit at index or -1 if error occurs
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index > 32)
+return (-1)
+
+if ((n & (1 << index)) == 0)
+return (0);
+
+return (1);
+}
