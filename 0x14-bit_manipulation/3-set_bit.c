97df05b5db8ba6615472d24fda80d531b89b6b67
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-05 17:24:23 +0000

    set value of a bit to 1 at a given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *get_bit - finds value of a bit at a given position
+ *@n: pointer the value to check
+ *@index: position to get the value at
+ *Return: value of bit at index or -1 if error occurs
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 32)
+return (-1);
+    
+*n ^= (1 << index);
+  
+return 1;
+}
