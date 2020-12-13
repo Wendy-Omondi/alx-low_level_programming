20f9d9d124e997bbeb977f290abdfad49e38ace9
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-13 14:49:47 +0000

    print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - prints all elements of a list
+ *@h: head of the list
+ *Return: Number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+int i; /*counter*/
+i = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
+}
