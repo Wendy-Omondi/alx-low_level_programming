86ee36773f2ff04f2b055f280a2766863554f084
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-31 09:30:50 +0000

    printf change

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,21 @@
-#include <stdio.h>            
-                              
-/**                           
- *main - Entry point.         
- *Return: Success             
- */                            
-int main(void)                
-{                             
-int i;                        
-int sum = 0;                  
-                              
-for (i = 1; i < 1024; i++)    
+#include <stdio.h>
+
+/**
+*main - Entry point.
+ *Return: Success
+ */
+int main(void)
+{
+int i;
+int sum = 0;
+
+for (i = 1; i < 1024; i++)
 {
 
 if (i % 3 == 0  || i % 5 == 0)
-sum += i;                     
+sum += i;
 
 }
-putchar(sum);
+printf("%d\n", sum);
 return (0);
 }
-
