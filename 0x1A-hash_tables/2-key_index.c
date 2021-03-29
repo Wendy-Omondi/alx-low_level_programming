ffa406d3cb548af6f1e61bc6de07022873cb09c4
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-03-29 08:58:46 +0000

    add return documentation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,6 +4,7 @@
  *key_index - gives you the index of a key
  *@key: the key
  *@size: size of the array of the hash table
+ *Return: key index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
