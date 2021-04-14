911a08c006be856c9b8f8137a690649e121f7436
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-14 09:58:50 +0000

    fix bettty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 {
 char x;
 int i;
-    
-for(i = 0; i <= 9; i++)
+
+for (i = 0; i <= 9; i++)
 {
-for(x = 'a'; x <= 'z'; x++)
+for (x = 'a'; x <= 'z'; x++)
 _putchar(x);
 _putchar('\n');
 }
